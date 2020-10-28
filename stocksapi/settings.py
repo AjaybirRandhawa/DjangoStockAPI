d9e3251f080b4a31f80822305a8879c02a3854ad
AjaybirRandhawa <66536527+AjaybirRandhawa@users.noreply.github.com>
2020-10-28 00:40:45 -0400

    Update settings.py

--- stocksapi/settings.py
+++ stocksapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8auh_$3kg9@$944@g8pcv)t-c3x7(gfc-v_qls#k_25+p_@vr='
+SECRET_KEY = '[REDACTED]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
